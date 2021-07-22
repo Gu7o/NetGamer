15c20e7d477ef14a06fa7312971a5a1b20d94c0c
Gu7o <gustavovv3@gmail.com>
2021-07-22 15:27:30 -0300

    adicao do app.js que faz com que ao clickar nas figuras o site e redirecionado para os respectivos jogos

---
+++ js/app.js
@@ -0,0 +1,20 @@
+let undertale = document.getElementById("undertale");
+/* As funções a seguir tem como objetivo direcionar para pagina dos jogos quando o usuario clica na figura */
+function trocaUnder(){
+    window.location.replace("https://undertale.com/");
+}
+
+function trocaDota(){
+    window.location.replace("https://www.dota2.com/home");
+}
+
+function trocaDoom(){
+    window.location.replace("https://store.steampowered.com/app/379720/DOOM/");
+}
+function trocaMario(){
+    window.location.replace("https://www.nintendo.com/games/detail/mario-luigi-superstar-saga-bowsers-minions-3ds/");
+}
+function trocaBorder(){
+    window.location.replace("https://borderlands.com/pt-BR/");
+}
+
